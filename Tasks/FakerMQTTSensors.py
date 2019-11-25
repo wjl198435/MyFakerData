d2456c727baee98d0cdccb846e97a0a5f8b626cf
wangjinliang <wjl198435@163.com>
2019-11-25 16:09:45 +0800

    完成mqtt sensor 设备发现、数据上报

--- Tasks/FakerMQTTSensors.py
+++ Tasks/FakerMQTTSensors.py
@@ -5,6 +5,33 @@ from utils.logger import info, setInfo,debug,setDebug
 from config import MQTT_DIS_PREFIX
 from Scheduler.scheduler import SchedulerEngine
 import datetime
+import random
+from threading import Thread, Event
+from time import strftime, localtime, tzset, time, sleep
+
+
+
+class TimerThread(Thread):
+    """Class to run a function on a thread at timed intervals"""
+
+    def __init__(self, function, interval, initial_delay=0):
+        """Set function to run at intervals and start thread"""
+        Thread.__init__(self)
+        self.setDaemon(True)
+        self.function = function
+        self.interval = interval
+        self.initial_delay = initial_delay
+        self.start()
+
+    def run(self):
+        """Run function at intervals"""
+        sleep(self.initial_delay)
+        while True:
+            try:
+                self.function()
+                sleep(self.interval)
+            except:
+                exception("TimerThread Unexpected error")
 
 class FakeMQSensors(object):
     def __init__(self, serverclient = None,session = getIotDataBaseSession(),company_id = 6):
@@ -18,30 +45,67 @@ class FakeMQSensors(object):
 
         # self.add_temperature_sensors()
 
-        self.schedulerEngine = SchedulerEngine(self, 'client_scheduler')
+        # self.schedulerEngine = SchedulerEngine(self, 'client_scheduler')
+        #
+        # self.add_schedule_job()
 
-        self.add_schedule_job()
+        # self.add_sensors()
+        TimerThread(self.do_faker_sensor,5, initial_delay=5)
+
+
+    def RunAction(self, action):
+        debug('RunAction:' + action)
+        self.get_sensors()
+        eval(action)()
+        return True
 
     def do_faker_sensor(self):
         debug("do_faker_sensor")
+        self.get_sensors()
+        # sub_sensors=np.random.choice(self.sensors,10,replace=False)
+        # sub_sensors = random.choice(self.sensors)
+        for i in range(10) :
+            # debug("do_faker_sensor:{}".format(i))
+            sensor =  random.choice(self.sensors)
+            faker_sensor_topic = "sensor/{}/{}/state".format(sensor[-1],sensor.sn)
+
+            if sensor.domain == "temperature":
+                faker_sensor_message = {"temperature" : round(random.uniform(10,40) ,1)}
+            if sensor.domain == "humidity":
+                faker_sensor_message = {"temperature" : round(random.uniform(1,99) ,1)}
+            if sensor.domain == "aqi":
+                faker_sensor_message = {"temperature": round(random.uniform(1,1999) ,1)}
+            if sensor.domain == "luminance":
+                faker_sensor_message = {"temperature" : round(random.uniform(1,49999) ,1)}
+            if sensor.domain == "NH3":
+                faker_sensor_message = {"temperature" : round(random.uniform(1,999) ,1)}
+
+            self._serverclient.EnqueuePacket(faker_sensor_message ,faker_sensor_topic)
+
 
     def add_schedule_job(self):
         now = datetime.datetime.strftime(datetime.datetime.utcnow(), '%Y-%m-%dT%H:%M:%S.%fZ')
-        event = {'id':'faker_sensor', 'title':'faker_sensors_minute', 'actions':['self.do_faker_sensor'], 'config':{'type':'interval','unit':'minute', 'interval':5,'start_date':now}}
+        event = {'id':'faker_sensor', 'title':'faker_sensors_minute', 'actions':['self.do_faker_sensor'], 'config':{'type':'interval','unit':'second', 'interval':300,'start_date':now}}
         self.schedulerEngine.add_scheduled_event(event)
         return event
      #/* select  concat('room', FLOOR(1 + (RAND() * 10))); */
-    def add_temperature_sensors(self):
 
-        self.sensors = self.dbsession.query(Sensor.domain,Sensor.sn,Sensor.unit,SensorInfo.loc,Company.english_name).\
-            join(SensorInfo).join(Company).\
-            filter(Sensor.company_id==str(self._company_id)).filter(Sensor.domain!='switch').filter(Sensor.domain!='fans').all()
+    def get_sensors(self):
+        if self.sensors is None:
+            self.sensors = self.dbsession. \
+                query(Sensor.domain,Sensor.sn,Sensor.unit,SensorInfo.loc,Company.english_name). \
+                join(SensorInfo).join(Company). \
+                filter(Sensor.company_id==str(self._company_id)). \
+                filter(Sensor.domain!='switch').filter(Sensor.domain!='fans').all()
 
+
+    def add_sensors(self):
+        self.get_sensors()
         for sensor in self.sensors:
             sensor_topic = "sensor/{}/{}/config".format(sensor[-1],sensor.sn)
             sensor_config={
                 "device_class": "temperature",
-                "name": str(sensor.domain+"_"+sensor[3]),
+                "name": str(sensor[3]+"_"+sensor.domain),
                 "state_topic": "{}/sensor/{}/{}/state".format(MQTT_DIS_PREFIX,sensor[-1],sensor.sn),
                 "unit_of_measurement": sensor.unit,
                 "value_template": "{{ value_json.temperature}}"
